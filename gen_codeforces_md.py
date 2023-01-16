cbd0aa39bab8e274688d99bd10ce6da0d7c3c237
jirayuwat12 <49447426+jirayuwat12@users.noreply.github.com>
2023-01-16 12:38:29 +0700

    add difficulty level scraping

--- gen_codeforces_md.py
+++ gen_codeforces_md.py
@@ -4,14 +4,28 @@ import os
 import re
 from tqdm import tqdm
 
+
 def get_file_name(path):
     file_list = os.listdir(path)
     file_list.sort()
     return file_list
+# scrap problem name from codeforces
+def web_scrap(file):
+    url = 'https://codeforces.com/problemset/problem/' + file[:-5] + '/' + file[-5]
+    # print(url)
+    import requests
+    from bs4 import BeautifulSoup
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    # get name
+    problem_name = soup.find('div', class_='title').text
+    problem_name = problem_name.replace('\n', '')
+    # get difficulty
+    difficulty = soup.find('div', id='sidebar').find('span', title='Difficulty').text.strip()[1:]
+    return problem_name, difficulty
 
 file_list = get_file_name('./CodeForces')
-# print to CodeForces.md
-
+# data preparation
 accepted_list = []
 ongoing_list = []
 for file in tqdm(file_list, desc='Finding file to print in CodeForces.md', total=len(file_list), unit='file', ncols = 100):
@@ -28,7 +42,7 @@ for file in tqdm(file_list, desc='Finding file to print in CodeForces.md', total
             accepted_list.append(file)
         else:
             ongoing_list.append(file)
-
+# print section
 with open('./CodeForces/CodeForces.md', 'w') as f:
     print('# CodeForces problem list', file=f)
     # table of content
@@ -41,11 +55,13 @@ with open('./CodeForces/CodeForces.md', 'w') as f:
         print('***No accepted problem***', file=f)
 
     for file in tqdm(accepted_list, desc='Accepted problem', total=len(accepted_list), unit='file', ncols = 100):
-        print(f'**{file[:-4]}**\n - code : [{file}](./CodeForces/{file})\n - link : [codeforces](https://codeforces.com/problemset/problem/{file[:-5]}/{file[-5]})\n', file=f)
+        name, difficulty = web_scrap(file)
+        print(f'**{file[:-5]} {name} ({difficulty})**\n - code : [{file}](./CodeForces/{file})\n - link : [codeforces](https://codeforces.com/problemset/problem/{file[:-5]}/{file[-5]})\n', file=f)
     
     print('## Ongoing', file=f)
     if len(ongoing_list) == 0:
         print('***No on-going problem***', file=f)
     
     for file in tqdm(ongoing_list, desc='On-going problem', total=len(ongoing_list), unit='file', ncols = 100):
-        print(f'**{file[:-4]}**\n - code : [{file}](./CodeForces/{file})\n - link : [codeforces](https://codeforces.com/problemset/problem/{file[:-5]}/{file[-5]})\n', file=f)
+        name, difficulty = web_scrap(file)
+        print(f'**{file[:-5]} {name} ({difficulty})**\n - code : [{file}](./CodeForces/{file})\n - link : [codeforces](https://codeforces.com/problemset/problem/{file[:-5]}/{file[-5]})\n', file=f)
