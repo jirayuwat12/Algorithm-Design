adaed307cfb2f33dfecc83ee97ffe5ae7819103b
jirayuwat12 <49447426+jirayuwat12@users.noreply.github.com>
2023-01-26 10:58:39 +0700

    update

--- push_github.bat
+++ push_github.bat
@@ -1,6 +1,16 @@
+@REM  delete all *.exe file in grader and codeforces folder
+python ./Utils/delete_exe.py
+
+@REM  update REAADME.md
+
+python gen_codeforces_md.py
+python gen_grader_md.py
+
+@REM  push to github
+
 git add .
 git commit -m "update"
 
 git push 
 
-REM Path: pull_github.bat
+@REM Path: pull_github.bat
